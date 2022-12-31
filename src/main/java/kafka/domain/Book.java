5958ec3dc0a741ccd3d7f5feaebbb8553d89038d
joy <joy@mail.com>
2022-12-31 13:31:32 +0700

    first attempt

---
+++ src/main/java/kafka/domain/Book.java
@@ -0,0 +1,19 @@
+package kafka.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Book {
+    private Integer bookId = 0;
+    private String bookName = "";
+    private String bookAuthor = "";
+
+
+}
